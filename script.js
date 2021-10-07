0ae19bbd63ea0ba496ee8f06d73630ee3cc36eef
JBG <jbg@embassies.com>
2021-10-07 15:42:26 +0200

    time from klaas

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const scripts = [
   [
     // Starting screen - JBG
     { txt: '', img: 'http://joinourspace.nl/school/start.png', time: 1 },
-    { txt: '', img: 'http://joinourspace.nl/school/0_intro.gif', time: 25 },
-    { txt: '', img: 'http://joinourspace.nl/school/1.gif', time: 60 },
+    { txt: '', img: 'http://joinourspace.nl/school/0_intro.gif', time: 35 },
+    { txt: '', img: 'http://joinourspace.nl/school/1.gif', time: 63 },
     { txt: '', img: 'http://joinourspace.nl/school/2.gif', time: 65 },
     { txt: '', img: 'http://joinourspace.nl/school/3.gif', time: 30 },
-    { txt: '', img: 'http://joinourspace.nl/school/4.gif', time: 85 },
+    { txt: '', img: 'http://joinourspace.nl/school/4.gif', time: 92 },
     { txt: '', img: 'http://joinourspace.nl/school/5.gif', time: 75 },
     { txt: '', img: 'http://joinourspace.nl/school/6.gif', time: 30 },
-    { txt: '', img: 'http://joinourspace.nl/school/7.gif', time: 75 },
+    { txt: '', img: 'http://joinourspace.nl/school/7.gif', time: 89 },
     { txt: '', img: 'http://joinourspace.nl/school/8.gif', time: 60 },
-    { txt: '', img: 'http://joinourspace.nl/school/9.gif', time: 110 },
+    { txt: '', img: 'http://joinourspace.nl/school/9.gif', time: 95 },
     // Ending screen - JBG
-    { txt: '', img: 'http://joinourspace.nl/school/10_outro.gif', time: 55 },
+    { txt: '', img: 'http://joinourspace.nl/school/11_outro.gif', time: 55 },
   ],
   // Group 2 - JBG
   /*
